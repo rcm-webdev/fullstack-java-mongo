defe1ce32d8b3c0573569ed12e5405c80305f6dc
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-27 10:44:50 -0700

    create full stack project with java springboot, mongodb and react

---
+++ src/main/java/com/rcmcodes/thecollection/TheCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.rcmcodes.thecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcmcodes/thecollection/TheCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcmcodes.thecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
