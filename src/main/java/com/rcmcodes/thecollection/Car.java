927076243e375a296a0bb49f09efbd3336f9052f
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-28 18:54:43 -0700

    update Car.java

--- src/main/java/com/rcmcodes/thecollection/Car.java
+++ src/main/java/com/rcmcodes/thecollection/Car.java
@@ -1,14 +1,22 @@
 package com.rcmcodes.thecollection;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-//this class will represent each car in the db
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//this class will represent each car in the db
 @Document(collection = "cars")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Car {
 
+    @Id
     private ObjectId id;
     private String toy_num;
     private String col_num;
