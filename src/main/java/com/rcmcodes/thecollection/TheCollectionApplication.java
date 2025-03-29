6352ee0818fb71a8b4530e34c8a5cb3b56e64ddf
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-28 18:55:22 -0700

    remove return, building api

--- src/main/java/com/rcmcodes/thecollection/TheCollectionApplication.java
+++ src/main/java/com/rcmcodes/thecollection/TheCollectionApplication.java
@@ -1,13 +1,16 @@
 package com.rcmcodes.thecollection;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class TheCollectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheCollectionApplication.class, args);
+		
 	}
+	
 
 }
